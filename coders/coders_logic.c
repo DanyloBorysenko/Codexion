e81f1d516960c4b3028d93b64ea25a731ff6b048
Danylo Borysenko <danborys@c1r5s4.42prague.com>
2026-04-27 18:27:27 +0200

    empty heap size check was added to take dongle func

--- coders/coders_logic.c
+++ coders/coders_logic.c
@@ -6,7 +6,7 @@
 /*   By: danborys <borysenkodanyl@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 12:27:48 by danborys          #+#    #+#             */
-/*   Updated: 2026/04/27 15:39:11 by danborys         ###   ########.fr       */
+/*   Updated: 2026/04/27 18:12:54 by danborys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,6 @@ int compile(coder_t *coder)
 }
 
 void insert_req(coder_t *coder, req_t req)
-
 {
 	dongle_t *first;
 	dongle_t *second;
@@ -118,20 +117,52 @@ void insert_req(coder_t *coder, req_t req)
 		second = coder->right_dng;
 	}
 	pthread_mutex_lock(&first->lock);
-	pthread_mutex_lock(&second->lock);
 	heap_insert(first->heap, req);
-	printf("INSERTed req, dongle %d, coder id %d, deadline %llu, arrivaltime %llu\n", first->num, req.coder_id, req.deadline, req.arr_time);
-	heap_insert(second->heap, req);
-	printf("INSERTed req, dongle %d, coder id %d, deadline %llu, arrivaltime %llu\n", second->num, req.coder_id, req.deadline, req.arr_time);
-	pthread_mutex_unlock(&second->lock);
 	pthread_mutex_unlock(&first->lock);
+	pthread_mutex_lock(&second->lock);
+	// printf("INSERTed req, dongle %d, coder id %d, deadline %llu, arrivaltime %llu\n", first->num, req.coder_id, req.deadline, req.arr_time);
+	heap_insert(second->heap, req);
+	// printf("INSERTed req, dongle %d, coder id %d, deadline %llu, arrivaltime %llu\n", second->num, req.coder_id, req.deadline, req.arr_time);
+	pthread_mutex_unlock(&second->lock);
+}
+
+int take_dongle(coder_t *coder, dongle_t *don)
+{
+	struct timespec ts;
+
+	pthread_mutex_lock(&don->lock);
+	while (!is_simul_finished(coder->simul))
+	{
+		if (don->heap->size == 0)
+		{
+			pthread_mutex_unlock(&don->lock);
+			return (0);
+		}
+		if (don->heap->reqs[0].coder_id == coder->id && !don->in_use && get_current_time() >= don->release)
+			break;
+		if (don->heap->reqs[0].coder_id == coder->id && !don->in_use)
+		{
+			ts = get_abs_time(don->release);
+			pthread_cond_timedwait(&don->cond, &don->lock, &ts);
+		}
+		else
+			pthread_cond_wait(&don->cond, &don->lock);
+	}
+	if (is_simul_finished(coder->simul))
+	{
+		pthread_mutex_unlock(&don->lock);
+		return (0);
+	}
+	don->in_use = 1;
+	log_event(coder->simul, coder->id, "has taken a dongle", get_current_time());
+	pthread_mutex_unlock(&don->lock);
+	return (1);
 }
 
 int	acquire_dongles(coder_t *coder)
 {
 	dongle_t	*first;
 	dongle_t	*second;
-	struct timespec ts;
 
 	if (coder->id % 2 == 0)
 	{
@@ -143,48 +174,10 @@ int	acquire_dongles(coder_t *coder)
 		first = coder->left_dng;
 		second = coder->right_dng;
 	}
-	pthread_mutex_lock(&first->lock);
-	while (!is_simul_finished(coder->simul))
-	{
-		if (first->heap->reqs[0].coder_id == coder->id && !first->in_use && get_current_time() >= first->release)
-			break;
-		if (first->heap->reqs[0].coder_id == coder->id && !first->in_use)
-		{
-			ts = get_abs_time(first->release);
-			pthread_cond_timedwait(&first->cond, &first->lock, &ts);
-		}
-		else
-			pthread_cond_wait(&first->cond, &first->lock);
-	}
-	if (is_simul_finished(coder->simul))
-	{
-		pthread_mutex_unlock(&first->lock);
+	if (!take_dongle(coder, first))
 		return (0);
-	}
-	first->in_use = 1;
-	log_event(coder->simul, coder->id, "has taken a dongle", get_current_time());
-	pthread_mutex_unlock(&first->lock);
-	pthread_mutex_lock(&second->lock);
-	while (!is_simul_finished(coder->simul))
-	{
-		if (second->heap->reqs[0].coder_id == coder->id && !second->in_use && get_current_time() >= second->release)
-			break;
-		if (second->heap->reqs[0].coder_id == coder->id && !second->in_use)
-		{
-			ts = get_abs_time(second->release);
-			pthread_cond_timedwait(&second->cond, &second->lock, &ts);
-		}
-		else
-			pthread_cond_wait(&second->cond, &second->lock);
-	}
-	if (is_simul_finished(coder->simul))
-	{
-		pthread_mutex_unlock(&second->lock);
+	if (!take_dongle(coder, second))
 		return (0);
-	}
-	second->in_use = 1;
-	log_event(coder->simul, coder->id, "has taken a dongle", get_current_time());
-	pthread_mutex_unlock(&second->lock);
 	return (1);
 }
 
